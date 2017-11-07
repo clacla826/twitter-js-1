54de4369c01587bb02332b734654154e6ff69a6a
Sara Rose Gallagher <srgallag@gmail.com>
2017-11-07 14:21:51 -0500

    separate index.js

--- app.js
+++ app.js
@@ -3,33 +3,20 @@ const nunjucks = require('nunjucks');
 const app = express();
 const routes = require('./routes');
 
-
-const router = express.Router();
-
-const tweetBank = require('./tweetBank');
-
-router.use('/', routes);
-
-router.get('/', function (req, res) {
-    let tweets = tweetBank.list();
-    res.render( 'index', { tweets: tweets } );
-  });
-  
-module.exports = router;
-
-
-
+nunjucks.configure('views', {noCache: true});
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render)
-nunjucks.configure('views', {noCache: true});
 
 
 
-// app.use(function(req, resp, next){
-//     console.log("\n hi this is the path requested: ", req.path, "this is the method: ", req.method)
-//     next();
-// })
+app.use(function(req, resp, next){
+        console.log("\n hi this is the path requested: ", req.path, "this is the method: ", req.method);
+        next();
+})
+
+
+app.use('/', routes);
 
 
 
@@ -39,15 +26,30 @@ nunjucks.configure('views', {noCache: true});
 
 
 app.listen(3000, function(){console.log("\n server listening")})
-var locals = {
-    title: 'An Example',
-    subtitle: 'Claudia',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var locals = {
+//     title: 'An Example',
+//     subtitle: 'Claudia',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
 
 
 
@@ -76,3 +78,15 @@ var locals = {
 // nunjucks.render('index.html', locals, function (err, output) {
 //     console.log(output);
 // });
+
+// nunjucks.render('index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+// app.get('/', function(req, res, next){
+//     res.render('index.html', locals, function(err, html){
+//         res.send(html);
+//     })
+
+//     //res.send("Hey There")
+
+// })
