8efb89b5e8660102fed99390e5425d83f3800cd8
Sara Rose Gallagher <srgallag@gmail.com>
2017-11-07 12:57:04 -0500

    currently crashing

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
+const routes = require('./routes');
 
 
+const router = express.Router();
+
+const tweetBank = require('./tweetBank');
+
+router.use('/', routes);
+
+router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+    res.render( 'index', { tweets: tweets } );
+  });
+  
+module.exports = router;
+
+
+
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render)
+nunjucks.configure('views', {noCache: true});
+
+
+
+// app.use(function(req, resp, next){
+//     console.log("\n hi this is the path requested: ", req.path, "this is the method: ", req.method)
+//     next();
+// })
+
+
+
+
+
+
+
+
+app.listen(3000, function(){console.log("\n server listening")})
 var locals = {
     title: 'An Example',
     subtitle: 'Claudia',
@@ -14,22 +50,10 @@ var locals = {
 };
 
 
-app.set('view engine', 'html');
-app.engine('html', nunjucks.render)
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
-});
 
-
-
-app.use(function(req, resp, next){
-    console.log("\n hi this is the path requested: ", req.path, "this is the method: ", req.method)
-    next();
-})
 
 // app.get('/', function(req, res, next){
-//     res.render('index', function(err, html){
+//     res.render('index.html', locals, function(err, html){
 //         res.send(html);
 //     })
 
@@ -37,20 +61,18 @@ app.use(function(req, resp, next){
 
 // })
 
-app.get('/news', function(req, res, next){
-    res.send("It's all fake news")
-})
+// app.get('/news', function(req, res, next){
+//     res.send("It's all fake news")
+// })
 
-app.get('/sara', function(req, res, next){
-    res.send("Hey I'm sara")
-})
+// app.get('/sara', function(req, res, next){
+//     res.send("Hey I'm sara")
+// })
 
-app.get('/claudia', function(req, res, next){
-    res.send("Hey I'm Claudia")
-})
+// app.get('/claudia', function(req, res, next){
+//     res.send("Hey I'm Claudia")
+// })
 
-
-
-
-
-app.listen(3000, function(){console.log("\n server listening")})
+// nunjucks.render('index.html', locals, function (err, output) {
+//     console.log(output);
+// });
