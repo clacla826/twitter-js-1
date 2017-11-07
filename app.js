057c3cfe144037a4bf8d776aaff90c40e81fa98b
Sara Rose Gallagher <srgallag@gmail.com>
2017-11-07 10:39:07 -0500

    with some reqs etc

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, resp, next){
+    console.log("\n hi this is the path requested: ", req.path, "this is the method: ", req.method)
+    next();
+})
+
 app.get('/', function(req, res, next){
     res.send("Hey There")
 })
 
+app.get('/news', function(req, res, next){
+    res.send("It's all fake news")
+})
+
+app.get('/sara', function(req, res, next){
+    res.send("Hey I'm sara")
+})
+
+app.get('/claudia', function(req, res, next){
+    res.send("Hey I'm Claudia")
+})
 
 
 app.listen(3000, function(){console.log("\n server listening")})
